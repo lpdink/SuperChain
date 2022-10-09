2de7225c6ab1d26d58a3a950fdbe801f8f10ca97
lpdink <lpdink@qq.com>
2022-10-09 16:11:22 +0000

    add async udp client and server of standard lib

---
+++ test/async_udp_client.py
@@ -0,0 +1,58 @@
+"""
+Author: lpdink
+Date: 2022-10-09 12:25:19
+LastEditors: lpdink
+LastEditTime: 2022-10-09 15:57:03
+Description: reference from https://docs.python.org/zh-cn/3/library/asyncio-protocol.html，官方文档。
+令人诧异，EchoClientProtocol实际上被要求实现asyncio.protocols.DatagramProtocol的必要方法，但文档中没有进行继承。
+The user should implement this interface.  They can inherit from
+    this class but don't need to.  The implementations here do
+    nothing (they don't raise exceptions).
+"""
+import asyncio
+
+
+class EchoClientProtocol:
+    def __init__(self, message, on_con_lost):
+        self.message = message
+        self.on_con_lost = on_con_lost
+        self.transport = None
+
+    def connection_made(self, transport):
+        self.transport = transport
+        print('Send:', self.message)
+        self.transport.sendto(self.message.encode())
+
+    def datagram_received(self, data, addr):
+        print("Received:", data.decode())
+
+        print("Close the socket")
+        self.transport.close()
+
+    def error_received(self, exc):
+        print('Error received:', exc)
+
+    def connection_lost(self, exc):
+        print("Connection closed")
+        self.on_con_lost.set_result(True)
+
+
+async def main():
+    # Get a reference to the event loop as we plan to use
+    # low-level APIs.
+    loop = asyncio.get_running_loop()
+
+    on_con_lost = loop.create_future()
+    message = "Hello World!"
+
+    transport, protocol = await loop.create_datagram_endpoint(
+        lambda: EchoClientProtocol(message, on_con_lost),
+        remote_addr=('127.0.0.1', 9999))
+
+    try:
+        await on_con_lost
+    finally:
+        transport.close()
+
+
+asyncio.run(main())
---
+++ test/async_udp_server.py
@@ -0,0 +1,41 @@
+"""
+Author: lpdink
+Date: 2022-10-09 16:03:12
+LastEditors: lpdink
+LastEditTime: 2022-10-09 16:04:20
+Description: UDP服务器，reference from https://docs.python.org/zh-cn/3/library/asyncio-protocol.html
+"""
+import asyncio
+
+
+class EchoServerProtocol:
+    def connection_made(self, transport):
+        self.transport = transport
+
+    def datagram_received(self, data, addr):
+        message = data.decode()
+        print('Received %r from %s' % (message, addr))
+        print('Send %r to %s' % (message, addr))
+        self.transport.sendto(data, addr)
+
+
+async def main():
+    print("Starting UDP server")
+
+    # Get a reference to the event loop as we plan to use
+    # low-level APIs.
+    loop = asyncio.get_running_loop()
+
+    # One protocol instance will be created to serve all
+    # client requests.
+    transport, protocol = await loop.create_datagram_endpoint(
+        lambda: EchoServerProtocol(),
+        local_addr=('127.0.0.1', 9999))
+
+    try:
+        await asyncio.sleep(3600)  # Serve for 1 hour.
+    finally:
+        transport.close()
+
+
+asyncio.run(main())
